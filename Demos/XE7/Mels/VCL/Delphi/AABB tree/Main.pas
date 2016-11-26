bad4dae37632876a907c07ceeccffeddc5f6d8d1
Jean-Milost Reymond <jean_milost@hotmail.com>
2016-11-26 09:36:31 -0300

    - fixed: error dialog message was shown incorrectly, all XE7 demo - cleaned: code and comments

--- Demos/XE7/Mels/VCL/Delphi/AABB tree/Main.pas
+++ Demos/XE7/Mels/VCL/Delphi/AABB tree/Main.pas
@@ -198,7 +198,7 @@ begin
     // initialize OpenGL
     if (not TQROpenGLHelper.EnableOpenGL(Handle, m_hDC, m_hRC)) then
     begin
-        MessageDlg('OpenGL could not be initialized.\r\n\r\nApplication will close.',
+        MessageDlg('OpenGL could not be initialized.' + #13#10#13#10 + 'Application will close.',
                    mtError,
                    [mbOK],
                    0);
